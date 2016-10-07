dc5c898a4861645da5b8b65f048f8b6105119555
S Daniel Zafar <daniel.zafar@navigant.com>
2016-10-07 18:19:55 -0500

    Made plotting batchable!

--- Vizualise.bat
+++ Vizualise.bat
@@ -1,4 +1,4 @@
 @echo on
 CD /D Y:\MA Utilities\Residential\RES 1 -Residential Baseline Study\Analysis\NILM\SparseNILM
-Rscript --vanilla disagg_viz.R 
+Rscript --vanilla disagg_viz.R AMPds_heatpump_furnace_dryer_noise_3st
 pause
--- disagg_viz.R
+++ disagg_viz.R
@@ -1,5 +1,11 @@
 # This script visualizes the results of disagg_NAV
 
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  stop("You need to add the out csv file name after the program as an arg", call.=FALSE)
+}
+
 require(dplyr)
 require(tidyr)
 require(ggplot2)
@@ -7,36 +13,33 @@ require(ggplot2)
 cat(paste(getwd(),"\n"))
 
 # name of the csv from disagg_NAV
-file_name = "AMPds_heatpump_furnace_dryer_noise_3st"
-house_data = "AMPdsR1_1min_A_.csv"
+#file_name = "AMPds_heatpump_furnace_dryer_noise_3st" # this was replaced by the trailing args
+file_name = args[1]
 frac = 1/200
 
 # name of the whole-house comparison data
-house <- read.csv(file.path("for_dissag",house_data))
-
 cat(paste0("Reading in ",file_name,".csv...\n"))
 data <- read.csv(file.path("csvs",paste0(file_name,".csv")))
 
 labels = ls(data)[ls(data) != "X"]
-glimpse(data %>%
-  select_(.dots = labels))
 
 cat("Constructing plot...\n")
-plot <- house %>% 
-  slice(-1) %>%
-  bind_cols(data) %>%
-  select_("TimeStamp","WHE", .dots = labels) %>%
-  gather_("Meter","Demand",c("WHE",labels))
+cat("\tLabels being plotted are:",labels)
 
+plot <- data %>%
+  select_(.dots = labels) %>%
+  mutate(TimeStamp = 1:nrow(data)) %>%
+  gather_("Meter","Demand",c(labels)) 
+  
 a <- plot %>% 
   filter(TimeStamp < (diff(range(TimeStamp))*frac+range(TimeStamp)[1])) %>%
   ggplot(aes(TimeStamp, Demand, color = Meter)) +
   geom_line()
 
-cat(paste0("Saving plot to: ",file.path("Visualization",paste0(file_name,".png...\n"))))
+cat(paste0("\nSaving plot to: ",file.path("Visualization",paste0(file_name,".png...\n"))))
 png(file.path("Visualization",paste0(file_name,".png")),
     height = 3, width = 8, units = "in", res = 300)
 a
 dev.off()
 
-cat("Completed Successfully!\n")
+cat("\nCompleted Successfully!\n")
